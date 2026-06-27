c761092aed34a34fa2f2c41eb064ca1ecd48a98c
Szymon <szymon.osinski.2009@gmail.com>
2026-06-27 13:03:07 +0200

    feat: getAvailableLangs = /lang/{video_id}

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import VideoUnavailable, TranscriptsDisabled
 
 app = FastAPI()
 
@@ -7,6 +9,33 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "test"}
+
+
+@app.get("/lang/{video_id}")
+def getAvailableLangs(video_id: str):
+    try:
+        ytt_api = YouTubeTranscriptApi()
+        transcript_list = ytt_api.list(video_id)
+
+        def clean_language(name: str) -> str:
+            return name
+            # return name.split("(")[0].strip()
+
+        result = {}
+
+        for t in transcript_list:
+            language_code = t.language_code
+            if language_code not in result:
+                result[language_code] = {
+                    "language": clean_language(t.language),
+                    "language_code": language_code,
+                }
+
+        return list(result.values())
+    except VideoUnavailable:
+        return []
+    except TranscriptsDisabled:
+        return []
 
 
 if __name__ == "__main__":
